40a1245a10937eac45ee524e7fe47b10d959efd3
Di Guan <dguan@purdue.edu>
2021-07-27 21:19:36 +0800

    first commit try

---
+++ src/main/java/com/test/testForSpringMVC.java
@@ -0,0 +1,11 @@
+package com.test;
+
+/**
+ * @author Di Guan on 7/27/2021 9:18 PM
+ */
+
+public class testForSpringMVC {
+    public static void main(String[] args) {
+        System.out.println("test for spring SSM");
+    }
+}
